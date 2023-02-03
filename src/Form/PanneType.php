3f5fd02ffa112e76213b3ac6da6204419c84e529
LionelCoutinot <69722628+LionelCoutinot@users.noreply.github.com>
2023-02-03 19:05:40 +0100

    Update PanneType.php

--- src/Form/PanneType.php
+++ src/Form/PanneType.php
@@ -14,7 +14,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PanneType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void  /* Définition des divers types de champs pour le formulaire de commande et des paramètres qui leur sont assignés si besoin */
+    public function buildForm(FormBuilderInterface $builder, array $options): void  /* Définition des divers types de champs pour le formulaire d'ajout de panne et des paramètres qui leur sont assignés si besoin */
     {
         $builder
         ->add('machine')
